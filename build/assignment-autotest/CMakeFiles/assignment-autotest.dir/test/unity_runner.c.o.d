2818afcd09fdeaca5e4c6e4e8af35a5ac2de98de
Benwafdy <107740350+AhmedAdelWafdy7@users.noreply.github.com>
2023-08-06 19:29:27 +0300

    Delete build directory

--- build/assignment-autotest/CMakeFiles/assignment-autotest.dir/test/unity_runner.c.o.d
+++
@@ -1,3 +0,0 @@
-assignment-autotest/CMakeFiles/assignment-autotest.dir/test/unity_runner.c.o: \
- /home/benwafdy/aesd-assignments/assignment-autotest/test/unity_runner.c \
- /usr/include/stdc-predef.h
