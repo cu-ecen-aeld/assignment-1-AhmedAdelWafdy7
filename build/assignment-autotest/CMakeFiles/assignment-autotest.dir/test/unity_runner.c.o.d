b89b6d948e1a8b0a76190a957e7d722511ccee7a
AhmedAdelWafdy7 <fandes99@gmail.com>
2023-08-12 16:24:32 +0300

    Add

---
+++ build/assignment-autotest/CMakeFiles/assignment-autotest.dir/test/unity_runner.c.o.d
@@ -0,0 +1,3 @@
+assignment-autotest/CMakeFiles/assignment-autotest.dir/test/unity_runner.c.o: \
+ /home/benwafdy/Desktop/assignment-1-AhmedAdelWafdy7/assignment-autotest/test/unity_runner.c \
+ /usr/include/stdc-predef.h
